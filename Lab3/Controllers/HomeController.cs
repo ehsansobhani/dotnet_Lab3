7997ac732cb4d0ce1b55bc1774cacf1a12f49a65
ehsansobhani <e.sobhani2000@gmail.com>
2022-06-26 09:27:56 -0700

    Add project files.

---
+++ Lab3/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Lab3.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Lab3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+
+
+
+
+
+
+        public IActionResult SongForm() => View();
+
+        [HttpPost]
+        public IActionResult Sing()
+        {
+            // you will complete this
+
+            
+            ViewBag.numberOfBottels = Request.Form["numberOfBottels"];
+            ViewData["numberOfBottels"] = Request.Form["numberOfBottels"];
+            return View();
+        }
+
+        public IActionResult CreatePerson() => View();
+
+        [HttpPost]
+        public IActionResult DisplayPerson(Person person)
+        {
+            // you will complete this
+            return View(person);
+        }
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+    }
+}
+
---
+++ Lab3/Models/Person.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lab3.Models
+{
+    public class Person
+    {
+        [Required]
+        [StringLength(255)]
+        [DataType(DataType.Text)]
+        public string firstName
+        {
+            get;
+            set;
+        }
+        [Required]
+        [StringLength(255)]
+        [DataType(DataType.Text)]
+        public string lastName
+        {
+            get;
+            set;
+        }
+        [Required]
+        public int age
+        {
+            get;
+            set;
+        }
+        [Required]
+        [StringLength(255)]
+        [EmailAddress]
+        public string emailAddress
+        {
+            get;
+            set;
+        }
+        [Required]
+        [StringLength(100)]
+        [DataType(DataType.Password)]
+        [BindProperty(Name = "pass")]
+        public string passWord
+        {
+            get;
+            set;
+        }
+        [StringLength(500)]
+        public string personDiscription
+        {
+            get;
+            set;
+        }
+
+    }
+}
